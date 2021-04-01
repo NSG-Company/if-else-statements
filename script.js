cd3a08ec6e08ce09b21be9491f015d0da37de86a
Newien Gopie <33331871+NSG-Company@users.noreply.github.com>
2021-04-02 00:24:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const age = 19;
+
+if (age > 19) {
+    console.log('enter!');
+}
+
+else if (age < 18) {
+    console.log('no entry!');
+}
+else if (age == 18) {
+    console.log("over 18");
+}
+else {
+    console.log("passed");
+}
+
+
+const isFemale = false;
+
+if (isFemale) {
+
+    console.log("you are invited");
+
+} else {
+
+    console.log("you're not invited");
+}
+
+const driverStatus = 'bob';
+
+if (driverStatus === 'bob') {
+    console.log("you can drive home");
+} else {
+    console.log("you cannot drive home");
+}
